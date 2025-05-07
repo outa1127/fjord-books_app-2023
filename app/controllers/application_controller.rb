b3529360028c28f38cdb41285e2d92380f9d8efc
outa1127 <ota.ono@livesense.co.jp>
2025-05-07 19:03:33 +0900

    ログアウト時のプログラムを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # /users/sign_up
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name post_code address introduction])
+  end
 end
