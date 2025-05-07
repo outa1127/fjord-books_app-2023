9ce9bbd9f21f2d7dfa2672015e52345d14a09161
outa1127 <ota.ono@livesense.co.jp>
2025-05-07 19:04:15 +0900

    不要なファイルをコメントアウト(最終的に削除する)

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -18,18 +18,18 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  protected
+  # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 
-  def after_sign_in_path_for
-    books_path
-  end
+  # def after_sign_in_path_for(_resource)
+  #   books_path
+  # end
 
-  def after_sign_out_path_for
-    new_user_session_path
-  end
+  # def after_sign_out_path_for(_resource_or_scope)
+  #   new_user_session_path
+  # end
 end
