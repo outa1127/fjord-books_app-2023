43c6599c689d2835ba4e0421b402ffcdc4e9ddc3
outa1127 <ota.ono@livesense.co.jp>
2025-05-09 17:57:13 +0900

    削除した時のメッセージを翻訳

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -52,7 +52,7 @@ class BooksController < ApplicationController
     @book.destroy
 
     respond_to do |format|
-      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
+      format.html { redirect_to books_url, notice: t('books.delete.book_deleted') }
       format.json { head :no_content }
     end
   end
