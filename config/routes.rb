4bfed881671bcac11d4a5e4eecb459d2269bbe35
outa1127 <ota.ono@livesense.co.jp>
2025-05-15 18:25:01 +0900

    追加したカラムも翻訳させる

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show]
   devise_for :users
   resources :books
+  resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
