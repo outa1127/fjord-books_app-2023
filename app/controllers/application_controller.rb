cbe315c72f18cf830b045fe4169593c5d0c1e4f0
outa1127 <ota.ono@livesense.co.jp>
2025-05-15 16:48:35 +0900

    フォームとリンクの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,17 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_account_update_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # /users/sign_up
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name post_code address introduction])
+  end
+
+  def configure_account_update_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name post_code address introduction])
   end
 end
