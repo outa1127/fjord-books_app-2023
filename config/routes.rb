1c4d7b5c0e23a11869f2e28a208cc99f870b197d
outa1127 <ota.ono@livesense.co.jp>
2025-05-16 17:43:22 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :reports
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  resources :books
+  resources :books do 
+    resources :comments, only: %i[create delete]
+  end
+  resources :reports do 
+    resources :comments, only: %i[create delete]
+  end
   resources :users, only: %i(index show)
 end
