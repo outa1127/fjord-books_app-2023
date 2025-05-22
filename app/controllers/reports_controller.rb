e9e741fe0a1eec9c3ca246814c2afb647e11ccc8
outa1127 <ota.ono@livesense.co.jp>
2025-05-22 16:29:32 +0900

    メンションに関する部分をメソッドとして切り出す

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -22,12 +22,7 @@ class ReportsController < ApplicationController
     @report = current_user.reports.new(report_params)
 
     if @report.save
-      if @report[:content].include?('http://localhost:3000/reports')
-        mentioned_report_ids = @report[:content].scan(%r{http://localhost:3000/reports/(\d+)}).flatten.map(&:to_i)
-        mentioned_report_ids.each do |mentioned_report_id|
-          @report.mentioning_relationships.create(mentioned_id: mentioned_report_id)
-        end
-      end
+      create_mentions(@report)
       redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
       render :new, status: :unprocessable_entity
@@ -38,12 +33,7 @@ class ReportsController < ApplicationController
     mentioned_report_ids = @report[:content].scan(%r{http://localhost:3000/reports/(\d+)}).flatten.map(&:to_i)
     if @report.update(report_params)
       new_mentioned_report_ids = @report[:content].scan(%r{http://localhost:3000/reports/(\d+)}).flatten.map(&:to_i)
-      add_mentioned_report_ids = new_mentioned_report_ids - mentioned_report_ids
-      add_mentioned_report_ids.each do |add_mentioned_report_id|
-        @report.mentioning_relationships.create(mentioned_id: add_mentioned_report_id)
-      end
-      delete_mentioned_report_ids = mentioned_report_ids - new_mentioned_report_ids
-      @report.mentioning_relationships.where(mentioned_id: delete_mentioned_report_ids).destroy_all
+      update_mentions(@report, mentioned_report_ids, new_mentioned_report_ids)
 
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
@@ -58,6 +48,26 @@ class ReportsController < ApplicationController
   end
 
   private
+
+  def create_mentions(report)
+    return unless report[:content].include?('http://localhost:3000/reports')
+
+    mentioned_report_ids = @report[:content].scan(%r{http://localhost:3000/reports/(\d+)}).flatten.map(&:to_i)
+    mentioned_report_ids.each do |mentioned_report_id|
+      @report.mentioning_relationships.create(mentioned_id: mentioned_report_id)
+    end
+  end
+
+  def update_mentions(report, mentioned_report_ids, new_mentioned_report_ids)
+    add_mentioned_report_ids = new_mentioned_report_ids - mentioned_report_ids
+    delete_mentioned_report_ids = mentioned_report_ids - new_mentioned_report_ids
+
+    add_mentioned_report_ids.each do |add_mentioned_report_id|
+      report.mentioning_relationships.create(mentioned_id: add_mentioned_report_id)
+    end
+
+    report.mentioning_relationships.where(mentioned_id: delete_mentioned_report_ids).destroy_all
+  end
 
   def set_report
     @report = current_user.reports.find(params[:id])
