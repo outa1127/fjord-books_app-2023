c9df39c206f96589f00da81bf88819b829f71528
outa1127 <ota.ono@livesense.co.jp>
2025-05-26 11:41:31 +0900

    reportsコントローラーのrespond_toの削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -23,12 +23,10 @@ class ReportsController < ApplicationController
   def create
     @report = current_user.reports.build(report_params)
 
-    respond_to do |format|
-      if @report.save
-        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human) }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
+    if @report.save
+      redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human)
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
