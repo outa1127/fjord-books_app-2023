b98b4d3db9bdd88f7c695c35055e5d621eb1faef
outa1127 <ota.ono@livesense.co.jp>
2025-05-26 20:19:52 +0900

    Railsでテストを書く

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -1,47 +1,50 @@
 # frozen_string_literal: true
 
 require 'application_system_test_case'
+require 'test_helper'
 
 class ReportsTest < ApplicationSystemTestCase
-  setup do
-    @report = reports(:one)
+  include Devise::Test::IntegrationHelpers
+  def setup
+    @report = reports(:test_report)
+    @updated_report = reports(:updated_report)
+
+    visit root_url
+    @user = users(:alice)
+    sign_in(@user)
   end
 
   test 'visiting the index' do
     visit reports_url
-    assert_selector 'h1', text: 'Reports'
+    assert_selector 'p', text: 'test日報'
   end
 
   test 'should create report' do
     visit reports_url
-    click_on 'New report'
+    click_on '日報の新規作成'
 
-    fill_in 'Content', with: @report.content
-    fill_in 'Title', with: @report.title
-    fill_in 'User', with: @report.user_id
-    click_on 'Create Report'
+    fill_in 'タイトル', with: @report.title
+    fill_in '内容', with: @report.content
+    click_on '登録する'
 
-    assert_text 'Report was successfully created'
-    click_on 'Back'
+    assert_text '日報が作成されました'
   end
 
   test 'should update Report' do
     visit report_url(@report)
-    click_on 'Edit this report', match: :first
+    click_on 'この日報を編集'
 
-    fill_in 'Content', with: @report.content
-    fill_in 'Title', with: @report.title
-    fill_in 'User', with: @report.user_id
-    click_on 'Update Report'
+    fill_in 'タイトル', with: @updated_report.title
+    fill_in '内容', with: @updated_report.content
+    click_on '更新する'
 
-    assert_text 'Report was successfully updated'
-    click_on 'Back'
+    assert_text '日報が更新されました。'
   end
 
   test 'should destroy Report' do
     visit report_url(@report)
-    click_on 'Destroy this report', match: :first
+    click_on 'この日報を削除'
 
-    assert_text 'Report was successfully destroyed'
+    assert_text '日報が削除されました。'
   end
 end
