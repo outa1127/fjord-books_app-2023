ddcea0320718774ef5116a944b380cb20a9b3558
outa1127 <ota.ono@livesense.co.jp>
2025-05-26 11:44:13 +0900

    reportコントローラーでdestroy!メソッドを使用するように修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -43,10 +43,10 @@ class ReportsController < ApplicationController
 
   # DELETE /reports/1 or /reports/1.json
   def destroy
-    @report.destroy
-
-    respond_to do |format|
-      format.html { redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human) }
+    if @report.destroy!
+      redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
+    else
+      redirect_to reports_url, notice: t('controllers.common.notice_failure_destroy', name: Report.model_name.human)
     end
   end
 
