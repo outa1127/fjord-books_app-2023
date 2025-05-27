b83ec0ae6ac6094b684f3cd207efc6f66df3e61e
outa1127 <ota.ono@livesense.co.jp>
2025-05-27 18:39:14 +0900

    destroyメソッドの修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,11 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
-  before_action :set_comment, only: %i[destroy]
+  # before_action :set_comment, only: %i[destroy]
 
   def destroy
-    @comment.destroy!
-    redirect_to @comment.commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
-  end
-
-  private
-
-  def set_comment
-    @comment = Comment.find(params[:id])
+    comment = Comment.find(params[:id])
+    comment.destroy!
+    redirect_to comment.commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
   end
 end
