8d39d8d88c374d9886c1baff3ab441e00a25a64a
outa1127 <ota.ono@livesense.co.jp>
2025-05-29 18:52:42 +0900

    モデルにバリデーションを追加

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -3,4 +3,6 @@
 class Mention < ApplicationRecord
   belongs_to :mentioned, class_name: 'Report', inverse_of: :mentioned_relationships
   belongs_to :mentioning, class_name: 'Report', inverse_of: :mentioning_relationships
+
+  validates :mentioned_id, uniqueness: { scope: :mentioning_id }
 end
