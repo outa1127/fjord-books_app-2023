a6f7e8d32e44846b31efd099675485b7ced296f5
outa1127 <ota.ono@livesense.co.jp>
2025-06-04 14:55:39 +0900

    静的値を使用するように修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -10,15 +10,12 @@ class ReportTest < ActiveSupport::TestCase
     @mentioned_report = reports(:mentioned_report)
   end
   test 'editable?' do
-    assert_equal true, @alice_report.editable?(@alice)
-  end
-
-  test 'not_editable?' do
-    assert_equal false, @alice_report.editable?(@bob)
+    assert @alice_report.editable?(@alice)
+    assert_not @alice_report.editable?(@bob)
   end
 
   test 'created_on' do
-    assert_equal Date.parse('2023-10-01 12:00:00'), @alice_report.created_on
+    assert_equal '2023-10-01'.to_date, @alice_report.created_on
   end
 
   test 'create report and check mentionn reflected' do
--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -7,7 +7,6 @@ class ReportsTest < ApplicationSystemTestCase
   include Devise::Test::IntegrationHelpers
   def setup
     @report = reports(:alice_report)
-    @updated_report = reports(:updated_report)
 
     visit root_url
     @user = users(:alice)
@@ -23,28 +22,40 @@ class ReportsTest < ApplicationSystemTestCase
     visit reports_url
     click_on '日報の新規作成'
 
-    fill_in 'タイトル', with: @report.title
-    fill_in '内容', with: @report.content
+    fill_in 'タイトル', with: 'テスト日報'
+    fill_in '内容', with: 'テストです'
     click_on '登録する'
 
     assert_text '日報が作成されました'
+    assert_text 'テスト日報'
   end
 
   test 'should update Report' do
     visit report_url(@report)
     click_on 'この日報を編集'
 
-    fill_in 'タイトル', with: @updated_report.title
-    fill_in '内容', with: @updated_report.content
+    fill_in 'タイトル', with: 'タイトル(編集済み)'
+    fill_in '内容', with: '本文(編集済み)'
     click_on '更新する'
 
     assert_text '日報が更新されました。'
+    assert_text 'タイトル(編集済み)'
   end
 
   test 'should destroy Report' do
-    visit report_url(@report)
+    visit reports_url
+    click_on '日報の新規作成'
+
+    fill_in 'タイトル', with: 'テスト日報'
+    fill_in '内容', with: 'テスト'
+    click_on '登録する'
+
+    assert_text 'テスト日報'
+
     click_on 'この日報を削除'
 
     assert_text '日報が削除されました。'
+
+    assert_no_text 'テスト日報'
   end
 end
