116c1b051430a07a1296c529be9447ea520687b7
outa1127 <ota.ono@livesense.co.jp>
2025-06-05 21:36:41 +0900

    メンションしている日報のid取得方法を修正

--- app/models/report.rb
+++ app/models/report.rb
@@ -34,16 +34,6 @@ class Report < ApplicationRecord
     false
   end
 
-  def create_mentions
-    collect_mentioned_report_ids.each do |mentioned_report_id|
-      mentioning_relationships.create!(mentioned_id: mentioned_report_id)
-    end
-  end
-
-  def collect_mentioned_report_ids
-    content.scan(%r{http://localhost:3000/reports/(\d+)}).flatten.map(&:to_i)
-  end
-
   def execute_update_procedure(report_params)
     ActiveRecord::Base.transaction do
       update!(report_params)
@@ -54,12 +44,23 @@ class Report < ApplicationRecord
     false
   end
 
+  private
+
+  def collect_mentioned_report_ids
+    content.scan(%r{http://localhost:3000/reports/(\d+)}).flatten.map(&:to_i)
+  end
+
+  def create_mentions
+    collect_mentioned_report_ids.each do |mentioned_report_id|
+      mentioning_relationships.create!(mentioned_id: mentioned_report_id)
+    end
+  end
+
   def update_mentions
-    old_mentioned_report_ids = mentioning_relationships.map(&:mentioned_id)
     new_mentioned_report_ids = collect_mentioned_report_ids
 
-    added_mentioned_report_ids = new_mentioned_report_ids - old_mentioned_report_ids
-    deleted_mentioned_report_ids = old_mentioned_report_ids - new_mentioned_report_ids
+    added_mentioned_report_ids = new_mentioned_report_ids - mentioning_report_ids
+    deleted_mentioned_report_ids = mentioning_report_ids - new_mentioned_report_ids
 
     added_mentioned_report_ids.each do |add_mentioned_report_id|
       mentioning_relationships.create!(mentioned_id: add_mentioned_report_id)
