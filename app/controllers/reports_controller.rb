e34f74f37fdece7442bb5324850055b34dabf0c0
outa1127 <ota.ono@livesense.co.jp>
2025-06-05 10:24:14 +0900

    日報と言及のセーブを同じ条件式に入れる

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -21,8 +21,7 @@ class ReportsController < ApplicationController
   def create
     @report = current_user.reports.new(report_params)
 
-    if @report.save
-      @report.create_mentions
+    if @report.save && @report.create_mentions
       redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
       render :new, status: :unprocessable_entity
